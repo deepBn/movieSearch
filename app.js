17f63370a9e78faf2f43f703a43a7ebbdaba2f3c
Deep Bn <deepBn9@gmail.com>
2018-02-28 22:24:16 +0530

    made basic search function

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+var request = require("request");
+
+app.get("/", function (req, res) {
+    res.render("search");
+});
+
+app.get("/results", function (req, res) {
+    var query = req.query.search;
+    var url = "http://www.omdbapi.com/?apikey=thewdb&s=" + query;
+    request(url, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            var data = JSON.parse(body);
+            res.render("results", {
+                data: data
+            });
+        }
+    });
+});
+
+app.listen("3000", function () {
+    console.log("Server running at port 3000");
+});
