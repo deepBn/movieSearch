b30d5638ab96aea61a9dacd3c331d0c83d1c2470
Rajdeep Barman <deepBn9@gmail.com>
2018-08-05 12:51:20 +0530

    added PORT option

--- app.js
+++ app.js
@@ -41,6 +41,6 @@ app.get("/results/details/:title", function(req, res) {
     });
 });
 
-app.listen("3000", function () {
+app.listen(process.env.PORT || "3000", function () {
     console.log("Server running at port 3000");
 });
